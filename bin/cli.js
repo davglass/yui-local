4f9593e821e0f626daa24e438c8062f810b5d20a
Dav Glass <davglass@gmail.com>
2012-01-27 10:34:08 -0600

    Output version with -v

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,26 @@
 var cli = require('cli'),
     local = require('yui-local');
 
-cli.enable('status', 'help');
+cli.enable('help');
 
 cli.parse({
+    version: ['v', 'Show app version'],
     port: ['p', 'The port number to assign: default 3000', 'number', 3000]
 });
 
+var showVersion = function() {
+    var path = require('path');
+    var meta = require(path.join('../package.json'));
+    var v = meta.version;
+    console.log('v' + v);
+    return v;
+};
+
 cli.main(function() {
     var opts = this.options;
+    if (opts.version) {
+        return showVersion();
+    }
     local.start(opts);
 });
 
