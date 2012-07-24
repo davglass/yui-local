4e8ff26b266c5c96fc771f0602c89bf9104f407d
Dav Glass <davglass@gmail.com>
2012-07-24 16:04:56 -0500

    Making it a little more programatic

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 var cli = require('cli'),
+    path = require('path'),
     local = require('yui-local');
 
 cli.enable('help');
 
 cli.parse({
     version: ['v', 'Show app version'],
+    build: ['b', 'Path to yui build dir', 'path', path.join(process.cwd(), 'build')],
     port: ['p', 'The port number to assign: default 3000', 'number', 3000]
 });
 
